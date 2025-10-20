ab7ca9db48666c039554ef9db5413b5f6818c855
Klavisi <klavisinaklavisi@gmail.com>
2025-10-20 22:28:04 +0500

    add questions logic

--- src/main/java/org/example/Token.java
+++ src/main/java/org/example/Token.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Token {
     // Замените на ваш реальный токен бота
-    public static final String BOT_TOKEN = "place_your_token_here";
+    public static final String BOT_TOKEN = "7943053072:AAEUnYdex-2Q25rW1R2f8lCPANlQYgcnFwk";
     public static final String BOT_USERNAME = "FunQuizBot";
 }
