f18cc33010f5eaf2c291d1fc4d669aed00e2c5f5
dasha <755784@mail.ru>
2025-12-16 05:07:20 +0500

    add shaker questions

--- src/main/java/org/example/model/QuizQuestion.java
+++ src/main/java/org/example/model/QuizQuestion.java
@@ -34,4 +34,45 @@ public class QuizQuestion {
     public boolean isCorrectAnswer(int answerIndex) {
         return answerIndex == correctAnswerIndex;
     }
+
+    public static class ShuffledQuestion {
+        private final String question;
+        private final List<String> options;
+        private final int correctAnswerIndex;
+        private final String image;
+
+        public ShuffledQuestion(String question, List<String> options, int correctAnswerIndex, String image) {
+            this.question = question;
+            this.options = options;
+            this.correctAnswerIndex = correctAnswerIndex;
+            this.image = image;
+        }
+
+        public String getQuestion() {
+            return question;
+        }
+
+        public List<String> getOptions() {
+            return options;
+        }
+
+        public int getCorrectAnswerIndex() {
+            return correctAnswerIndex;
+        }
+
+        public String getImage() {
+            return image;
+        }
+
+        public boolean isCorrectAnswer(int answerIndex) {
+            return answerIndex == correctAnswerIndex;
+        }
+
+        public String getCorrectAnswerText() {
+            if (correctAnswerIndex >= 0 && correctAnswerIndex < options.size()) {
+                return options.get(correctAnswerIndex);
+            }
+            return "Неизвестно";
+        }
+    }
 }
